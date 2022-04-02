c3862a44563219e6d7cb17e7ffde95e2b379f090
M Alam <m@alam.codes>
2022-04-02 15:30:53 -0400

    fix jest with babel

---
+++ test/utils.test.js
@@ -0,0 +1,21 @@
+import { getAdjacentDiagCoords } from "../lib/utils/pawnFn";
+
+describe("pawnFn", () => {
+  it("gets adjacent diagonal coords (to check for enemy pieces)", () => {
+    const inputCoords1 = [];
+    const inputColor1 = "w";
+    const expectedOutput1 = [];
+
+    expect(getAdjacentDiagCoords(inputCoords1, inputColor1)).toEqual(
+      expectedOutput1
+    );
+
+    const inputCoords2 = [];
+    const inputColor2 = "b";
+    const expectedOutput2 = [];
+
+    expect(getAdjacentDiagCoords(inputCoords2, inputColor2)).toEqual(
+      expectedOutput2
+    );
+  });
+});
