837536151ea371f188eec65d8a1d2a2325441610
M M Alam <m@alam.codes>
2022-04-10 20:33:20 -0400

    abstract away stuff;

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 import chess from "./lib/chess.js";
-import { createInitState } from "./lib/helpers.js";
 
-const chessDiv = document.getElementById("chess");
-
-const r = chess(chessDiv);
-
-const gameState = createInitState();
-
-r(gameState);
+chess(document.getElementById("chess"));
